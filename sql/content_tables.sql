a27b59c8b1c8760cab81adf2b07a9f4be131b739
Guilherme Sebastião <guivbsebas@gmail.com>
2024-04-27 17:51:56 -0300

    Implement SQL data loading functionality

---
+++ sql/content_tables.sql
@@ -0,0 +1,27 @@
+/* 
+
+1. Open pgAdmin
+2. In Object Explorer (left-hand pane), navigate to `sql_course` database
+3. Right-click `sql_course` and select `PSQL Tool`
+    - This opens a terminal window to write the following code
+4. Get the absolute file path of your csv files
+    1. Find path by right-clicking a CSV file in VS Code and selecting “Copy Path”
+5. Paste the following into `PSQL Tool`, (with the CORRECT file path)
+
+*/
+
+COPY company_dim
+FROM 'C:\Program Files\PostgreSQL\16\data\Datasets\sql_course\company_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY skills_dim
+FROM 'C:\Program Files\PostgreSQL\16\data\Datasets\sql_course\skills_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY job_postings_fact
+FROM 'C:\Program Files\PostgreSQL\16\data\Datasets\sql_course\job_postings_fact.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY skills_job_dim
+FROM 'C:\Program Files\PostgreSQL\16\data\Datasets\sql_course\skills_job_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
